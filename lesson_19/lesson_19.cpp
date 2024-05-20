59e2d5ee48a3ac46c4f6399a33a950c9bc49a39e
SEGARS <vim_branet@mail.ru>
2024-05-20 17:33:16 +0500

    база

---
+++ lesson_19/lesson_19.cpp
@@ -0,0 +1,25 @@
+﻿// Без этого вы не станете программистом! Найти ошибку в коде. Отладка. Как пользоваться отладчиком Урок #23 Видео - 30.
+
+#include<iostream>
+using namespace std;
+
+int Sum(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int a = 5;
+
+	int b = 6;
+
+	int c;
+
+	c = Sum(a, b);
+
+	cout << c << endl;
+}
