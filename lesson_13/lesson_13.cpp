726fc5ebc3028e17dc64984b06f9e7c76db95f85
SEGARS <vim_branet@mail.ru>
2024-05-20 17:15:44 +0500

    база

---
+++ lesson_13/lesson_13.cpp
@@ -0,0 +1,23 @@
+﻿// Цикл do while.Что это.Что делает.Пример.Синтаксис. Урок #15 - Видео №20.
+
+#include<iostream>
+using namespace std;
+
+//Цикл do while
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int a = 0;
+
+	do
+	{
+		cout << "Переменная а = " << a << endl;
+		a++;
+	} while (a < 10);
+	{
+		cout << "Переменная а = " << a << endl;
+		a++;
+	}
+}
